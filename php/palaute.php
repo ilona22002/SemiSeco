4a0cd478bdb59d63fe179a9748b7f2e53f8beb24
heidi-999 <Peltola99@gmail.com>
2023-02-15 11:10:52 +0200

    palaute.php laitettu toimimaan ja tulostamaan

--- php/palaute.php
+++ php/palaute.php
@@ -1,9 +1,9 @@
 <?php
-$etunimi=$_POST["etunimi"] ? $_POST["etunimi"]  : "";
-$sukunimi=$_POST["sukunimi"]  ? $_POST["sukunimi"]  : "";
-$puh=$_POST["puh"] ? $_POST["puh"]  : "";
-$email=$_POST["email"]  ? $_POST["email"]  : "";
-$palaute=$_POST["palaute"]  ? $_POST["palaute"]  : "";
+$etunimi=isset($_POST["etunimi"]) ? $_POST["etunimi"]  : "";
+$sukunimi=isset($_POST["sukunimi"])  ? $_POST["sukunimi"]  : "";
+$puh=isset($_POST["puh"]) ? $_POST["puh"]  : "";
+$email=isset($_POST["email"])  ? $_POST["email"]  : "";
+$palaute=isset($_POST["palaute"])  ? $_POST["palaute"]  : "";
 
 mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
 
@@ -15,4 +15,31 @@ catch(Exception $e){
     header("Location:../html/yhteysvirhe.html");
     exit;
 }
+
+if (!empty($palaute)) {
+
+    $sql="insert into palaute (etunimi, sukunimi, puhelinnumero, sahkoposti, palaute) values(?, ?, ?, ? ,?)";
+
+    //Valmistellaan sql-lause
+    $stmt=mysqli_prepare($yhteys, $sql);
+    //Sijoitetaan muuttujat oikeisiin paikkoihin
+    mysqli_stmt_bind_param($stmt, 'sssss', $etunimi, $sukunimi, $puh, $email, $palaute);
+    //Suoritetaan sql-lause
+    mysqli_stmt_execute($stmt);
+    header("Location:./palaute.php");
+    exit;
+}
+
+//kokeillaan tulostaa tietokannan sisältö niin tiedetään toimiiko koodi
+$tulos=mysqli_query($yhteys, "select * from palaute");
+print "<table border='1'>";
+
+while ($rivi=mysqli_fetch_object($tulos)){
+    print "<tr>";
+    print "<td>$rivi->id<td>$rivi->etunimi<td>$rivi->sukunimi<td>$rivi->puhelinnumero<td>$rivi->sahkoposti<td>$rivi->palaute";
+}
+print "</table>";
+
+
+mysqli_close($yhteys);
 ?>
