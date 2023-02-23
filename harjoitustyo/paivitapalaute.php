a11908046a916f654a7f858e6a645fe4230f1480
nelli22003 <nelli22003@student.hamk.fi>
2023-02-23 10:21:29 +0200

    muokkaa, julkinen, poista ja päivitä

---
+++ harjoitustyo/paivitapalaute.php
@@ -0,0 +1,42 @@
+<?php
+//Luetaan lomakkeelta tulleet tiedot funktiolla $_POST
+//jos syötteet ovat olemassa
+$id=isset($_POST["id"]) ? $_POST["id"] : "";
+$etunimi=isset($_POST["etunimi"]) ? $_POST["etunimi"] : "";
+$sukunimi=isset($_POST["sukunimi"]) ? $_POST["sukunimi"] : 0;
+$sahkoposti=isset($_POST["sahkoposti"]) ? $_POST["sahkoposti"] : 0;
+$palaute=isset($_POST["palaute"]) ? $_POST["palaute"] : 0;
+
+//Jos ei jompaa kumpaa tai kumpaakaan tietoa ole annettu
+//ohjataan pyyntö takaisin lomakkeelle
+if (empty($etunimi) || empty($sukunimi) || empty($id) || empty($sahkoposti) || empty($palaute)){
+    header("Location:./palaute.php");
+    exit;
+}
+
+mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
+// mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+try{
+    $yhteys=mysqli_connect("db", "root", "password", "ryhma17_palautteet");
+}
+catch(Exception $e){
+    header("Location:./palaute.php");
+    exit;
+}
+
+//Tehdään sql-lause, jossa kysymysmerkeillä osoitetaan paikat
+//joihin laitetaan muuttujien arvoja
+$sql="update palaute set etunimi=?, sukunimi=?, sahkoposti=?, palaute=?, where id=?";
+
+//Valmistellaan sql-lause
+$stmt=mysqli_prepare($yhteys, $sql);
+//Sijoitetaan muuttujat oikeisiin paikkoihin
+mysqli_stmt_bind_param($stmt, 'ssssi', $etunimi, $sukunimi, $sahkoposti, $palaute, $id);
+//Suoritetaan sql-lause
+mysqli_stmt_execute($stmt);
+//Suljetaan tietokantayhteys
+mysqli_close($yhteys);
+
+header("Location:./palaute.php");
+?>
